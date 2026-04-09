515778eeaa1803a908702703b0413d3f2fad2c18
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2026-04-09 09:53:17 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+function App() {
+  return <h1 className='p-4 bg-pink-100 rounded-x1 shadow'>Hello my frend</h1>;
+}
+ 
+
+export default App;
---
+++ src/services/api.ts
@@ -0,0 +1,12 @@
+const API ='https://api.imdbapi.dev';
+
+export const searchMovies = async (query: string, limit = 50) => {
+    const res = await fetch ('${API}/search/titles?query=${query}&limit=${limit}');
+    return res.json();
+}
+
+export const getMovieById = async (id: string) => {
+    const res = await fetch('${API}/titles/&(id)');
+    return res.json();
+
+}
